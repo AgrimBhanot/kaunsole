1ac599487b96bc6324a9c3bcee693dbbed69b879
m1rabil1z <abhirupmahato488@gmail.com>
2026-08-22 11:05:46 +0530

    update: palette can be indexed

--- roms/smb3/camera.c
+++ roms/smb3/camera.c
@@ -2,6 +2,7 @@
 #include "tile.h"
 #include "obj.h"
 #include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 
 #include "defs.h"
@@ -328,7 +329,12 @@ const uint32_t *active_map;
 uint8_t active_screen;
 uint8_t read_offset;
 uint8_t write_offset;
-uint16_t block_buffer[N_SCREENS][16][16];
+struct block_entry {
+    uint16_t id;
+    uint8_t palette;
+};
+
+struct block_entry block_buffer[N_SCREENS][16][16];
 
 
 void stream_screen(bool reverse) {
@@ -393,9 +399,11 @@ void stream_screen(bool reverse) {
                 }
 
                 if (vertical) {
-                    block_buffer[target_screen][i + y][j + x + o1_x] = block;
+                    block_buffer[target_screen][i + y][j + x + o1_x].id = block;
+                    block_buffer[target_screen][i + y][j + x + o1_x].palette = palette;
                 } else {
-                    block_buffer[target_screen][j + x + o1_x][i + y] = block;
+                    block_buffer[target_screen][j + x + o1_x][i + y].id = block;
+                    block_buffer[target_screen][j + x + o1_x][i + y].palette = block;
                 }
             }
         }
@@ -432,9 +440,6 @@ void load_map(const uint32_t *m) {
     stream_screen(0);
     stream_screen(0);
 }
-
-
-
 
 uint16_t guide_counter = 0;
 struct camera_guide *guide;
@@ -489,17 +494,19 @@ void camera_draw_sprite(struct sprite *sprite) {
 void camera_draw() {
     for (uint8_t y = 0; y < 16; y++) {
         for (uint8_t x = 0; x < 16; x++) {
-            uint16_t block = block_buffer[active_screen][y][x];
-            draw_tile(blocks[block][0], y * 16 - camera_y, x * 16 - camera_x, 0, 0);
-            draw_tile(blocks[block][1], y * 16 - camera_y + 8, x * 16 - camera_x, 0, 0);
-            draw_tile(blocks[block][2], y * 16 - camera_y, x * 16 - camera_x + 8, 0, 0);
-            draw_tile(blocks[block][3], y * 16 - camera_y + 8, x * 16 - camera_x + 8, 0, 0);
+            uint16_t block = block_buffer[active_screen][y][x].id;
+            uint8_t palette = block_buffer[active_screen][y][x].palette;
+            draw_tile(blocks[block][0], y * 16 - camera_y, x * 16 - camera_x, palette, 0);
+            draw_tile(blocks[block][1], y * 16 - camera_y + 8, x * 16 - camera_x, palette, 0);
+            draw_tile(blocks[block][2], y * 16 - camera_y, x * 16 - camera_x + 8, palette, 0);
+            draw_tile(blocks[block][3], y * 16 - camera_y + 8, x * 16 - camera_x + 8, palette, 0);
 
-            block = block_buffer[(active_screen + 1) % N_SCREENS][y][x];
-            draw_tile(blocks[block][0], y * 16 - camera_y, x * 16 - camera_x + 256, 0, 0);
-            draw_tile(blocks[block][1], y * 16 - camera_y + 8, x * 16 - camera_x + 256, 0, 0);
-            draw_tile(blocks[block][2], y * 16 - camera_y, x * 16 - camera_x + 256 + 8, 0, 0);
-            draw_tile(blocks[block][3], y * 16 - camera_y + 8, x * 16 - camera_x + 256 + 8, 0, 0);
+            block = block_buffer[(active_screen + 1) % N_SCREENS][y][x].id;
+            palette = block_buffer[(active_screen + 1) % N_SCREENS][y][x].palette;
+            draw_tile(blocks[block][0], y * 16 - camera_y, x * 16 - camera_x + 256, palette, 0);
+            draw_tile(blocks[block][1], y * 16 - camera_y + 8, x * 16 - camera_x + 256, palette, 0);
+            draw_tile(blocks[block][2], y * 16 - camera_y, x * 16 - camera_x + 256 + 8, palette, 0);
+            draw_tile(blocks[block][3], y * 16 - camera_y + 8, x * 16 - camera_x + 256 + 8, palette, 0);
         }
         // fprintf(stderr, "\n");
     }
